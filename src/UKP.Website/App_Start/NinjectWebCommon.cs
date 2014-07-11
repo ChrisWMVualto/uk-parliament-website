3ab98df018c9b30ec426f0ee69da263e8c9970d3
cairey <cairey@localhost>
2014-07-11 16:51:21 +0100

    moved

--- src/UKP.Website/App_Start/NinjectWebCommon.cs
+++ src/UKP.Website/App_Start/NinjectWebCommon.cs
@@ -15,28 +15,24 @@ namespace UKP.Web.App_Start
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
-        /// <summary>
-        /// Starts the application
-        /// </summary>
         public static void PreStart() 
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
+
+
+        public static void PostStart()
+        {
+        }
+
         
-        /// <summary>
-        /// Stops the application.
-        /// </summary>
         public static void Stop()
         {
             bootstrapper.ShutDown();
         }
         
-        /// <summary>
-        /// Creates the kernel that will manage your application.
-        /// </summary>
-        /// <returns>The created kernel.</returns>
         private static IKernel CreateKernel()
         {
             var kernel = new StandardKernel();
@@ -55,10 +51,6 @@ namespace UKP.Web.App_Start
             }
         }
 
-        /// <summary>
-        /// Load your modules or register your services here!
-        /// </summary>
-        /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
         }        
