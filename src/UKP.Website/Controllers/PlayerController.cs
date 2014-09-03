ca97a1e5a353cad90b3cc570c776c28f1e88db74
cairey <cairey@localhost>
2014-09-03 10:39:45 +0100

    legacy route

--- src/UKP.Website/Controllers/PlayerController.cs
+++ src/UKP.Website/Controllers/PlayerController.cs
@@ -21,17 +21,12 @@ namespace UKP.Website.Controllers
         [HttpGet]
         public virtual ActionResult LegacyPlayerRoute()
         {
-            //TODO: add support for in point timecode
-            if (Request.Url != null)
+            if(Request.Url != null)
             {
-                var decodedUrl = HttpUtility.UrlDecode(Request.Url.Query);
-                if(decodedUrl != null)
-                {
-                    var embedString = decodedUrl.Replace("?", "").Split(new[] { ' ' });
-                    var meetingId = embedString[0];
-                    var legacyVideo = _videoService.GetLegacyVideo(int.Parse(meetingId));
-                    return JavaScript(legacyVideo.LegacyEmbedCode);
-                }
+                var embedString = Request.QueryString.ToString().Split(' ', '+');
+                var meetingId = embedString[0];
+                var legacyVideo = _videoService.GetLegacyVideo(int.Parse(meetingId));
+                return JavaScript(legacyVideo.LegacyEmbedCode);
             }
 
             return new HttpStatusCodeResult(HttpStatusCode.NotFound);
