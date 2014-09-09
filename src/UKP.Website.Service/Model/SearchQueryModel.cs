154c25de381bf61cf3106414f5cf3313db3dff6c
danielsgroves <danielsgroves@localhost>
2014-09-09 11:12:00 +0100

    FromDate search support

--- src/UKP.Website.Service/Model/SearchQueryModel.cs
+++ src/UKP.Website.Service/Model/SearchQueryModel.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Date.Extensions;
 
 namespace UKP.Website.Service.Model
 {
@@ -8,14 +7,14 @@ namespace UKP.Website.Service.Model
         public string Keywords { get; set; }
         public string Tags { get; set; }
         public int? MemberId { get; set; }
-        public DateTime? Period { get; set; }
+        public DateTime Period { get; set; }
 
-        public SearchQueryModel(string keywords, string tags, int? memberId, string period)
+        public SearchQueryModel(string keywords, string tags, int? memberId, int period)
         {
             Keywords = keywords;
             Tags = tags;
             MemberId = memberId;
-            Period = period.FromISO8601String();
+            Period = DateTime.Today.AddDays(period * -1);
         }
     }
 }
