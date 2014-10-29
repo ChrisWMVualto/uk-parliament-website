db109b3c8b0287fbdf2190a7c08307e7b10fb52a
danielsgroves <danielsgroves@localhost>
2014-10-29 16:57:28 +0000

    Scroll walking up and down the tabs

---
+++ src/UKP.Website.Service/Model/EpgEventModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UKP.Website.Application;
+
+namespace UKP.Website.Service.Model
+{
+    public class EpgEventModel
+    {
+        public EventModel EventData { get; set; }
+
+        public EpgEventModel(EventModel eventData)
+        {
+            EventData = eventData;
+        }
+
+        public EpgEventModel()
+        {
+            
+        }
+
+        private int TemplateWidthInt()
+        {
+            var difference = EventData.DisplayEndDate.Subtract(EventData.DisplayStartDate);
+            return EventConstants.EPG_MINUTE_SIZE * (int)difference.TotalMinutes;
+        }
+
+        public string TemplateWidth()
+        {
+            return string.Format("{0}px", TemplateWidthInt());
+        }
+
+        public string TemplateLeftPosition()
+        {
+            var difference = EventData.DisplayStartDate.ToLocalTime().Subtract(EventData.DisplayStartDate.ToLocalTime().Date);
+            var left = EventConstants.EPG_MINUTE_SIZE * (int)difference.TotalMinutes;
+
+            return string.Format("{0}px", left);
+        }
+
+        public bool SmallItem()
+        {
+            return TemplateWidthInt() <= (90 * EventConstants.EPG_MINUTE_SIZE);
+        }
+    }
+}
