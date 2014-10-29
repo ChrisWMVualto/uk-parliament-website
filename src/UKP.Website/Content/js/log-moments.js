b559038a1a55246e6a1703903ee2390fe969aa19
cairey <chris.airey@vualto.com>
2014-10-29 12:31:26 +0000

    event page

---
+++ src/UKP.Website/Content/js/log-moments.js
@@ -0,0 +1,58 @@
+﻿var playerDateTime;
+var stackPos = 'top';
+
+function scrollStack() {
+    if ($(".stack").length) {
+        $('.stack').slimScroll({
+            railVisible: true,
+            railColor: '#ffffff',
+            railOpacity: 0.3,
+            color: '#ffffff',
+            size: '12px',
+            height: 'auto',
+            alwaysVisible: false,
+            start: stackPos
+        });
+
+
+        $('.stack').slimScroll().bind('slimscroll', function(e, pos){
+            stackPos = pos;
+        });
+    }
+}
+
+
+
+
+function updateStacks() {
+    if ($('#ContainsLogMoments').val() == "False") {
+        var stackUrl = $('#eventStackContainer').data("load-stack-url");
+        $.get(stackUrl, function (data) {
+            $('#eventStackContainer').html(data);
+            scrollStack();
+        });
+    }
+    var stackPollingInterval = parseInt($('#StackPollingInterval').val());
+   // setTimeout(updateStacks, stackPollingInterval);
+}
+
+
+function updateLogMoments() {
+    if ($('#ContainsLogMoments').val() == "True") {
+        var logUrl = $('#eventStackContainer').data("load-log-url");
+        $.get(logUrl, function (data) {
+            $('#eventStackContainer').html(data);
+        });
+    }
+
+    var stackPollingInterval = parseInt($('#StackPollingInterval').val());
+    setTimeout(updateLogMoments, stackPollingInterval);
+}
+
+
+
+
+$(function () {
+    updateStacks();
+    //updateLogMoments();
+});
