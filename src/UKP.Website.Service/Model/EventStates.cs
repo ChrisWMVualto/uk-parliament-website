010892c826c6c9f805871a0815058be79b11eaf5
danielsgroves <danielsgroves@localhost>
2014-10-30 10:14:23 +0000

    Fixed tab devancing bug

---
+++ src/UKP.Website.Service/Model/EventStates.cs
@@ -0,0 +1,53 @@
+﻿namespace UKP.Website.Service.Model
+{
+    public class EventStates
+    {
+        public EventStates(PlanningEventState planningState, RecordingEventState recordingState, RecordedEventState recordedState, PlayerEventState playerState)
+        {
+            PlanningState = planningState;
+            RecordingState = recordingState;
+            RecordedState = recordedState;
+            PlayerState = playerState;
+        }
+
+        public PlanningEventState PlanningState { get; private set; }
+        public RecordingEventState RecordingState { get; private set; }
+        public RecordedEventState RecordedState { get; private set; }
+        public PlayerEventState PlayerState { get; private set; }
+    }
+
+    public enum PlanningEventState
+    {
+        VOID = 0,
+        NEW = 1,
+        PROPOSED = 2,
+        CONFIRMED = 3,
+        STOP_DVR = 4
+    }
+
+    public enum RecordingEventState
+    {
+        VOID = 0,
+        IDLE = 1,
+        ERROR = 2,
+        RECORDING = 3,
+        COMPLETED = 4
+    }
+
+    public enum RecordedEventState
+    {
+        VOID = 0,
+        NEW = 1,
+        REVOKE = 2,
+        HOLD = 3,
+        BASIC = 4
+    }
+
+    public enum PlayerEventState
+    {
+        PRELIVE = 0,
+        LIVE = 1,
+        ARCHIVE = 2,
+        ERROR = 3
+    }
+}
---
+++ src/UKP.Website/Controllers/SearchController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using UKP.Website.Models;
+using UKP.Website.Models.Event;
+using UKP.Website.Models.Search;
+using UKP.Website.Service;
+using UKP.Website.Service.Model;
+
+
+namespace UKP.Website.Controllers
+{
+    public partial class SearchController : Controller
+    {
+        private readonly ISearchService _searchService;
+
+        public SearchController(ISearchService searchService)
+        {
+            _searchService = searchService;
+        }
+
+
+        [HttpGet]
+        public virtual ActionResult Index(string keywords, int? memberId, string member, string house, string business, string start, string end, int page = 1)
+        {
+            DateTime fromDate;
+            if(!DateTime.TryParse(start, out fromDate))
+            {
+                fromDate = DateTime.Today.AddMonths(-1);
+            }
+            DateTime toDate;
+            if(!DateTime.TryParse(end, out toDate))
+            {
+                toDate = DateTime.Today;
+            }
+
+            var searchModel = new SearchViewModel()
+                              {
+                                  Keywords = keywords,
+                                  MemberId = memberId,
+                                  House = house,
+                                  Business = business,
+                                  Start = fromDate,
+                                  End = toDate,
+                                  Member = member,
+                                  BusinessTags = new SelectList(_searchService.GetTags().Where(x => x.Category == "Business"), "DisplayTag", "DisplayTag"),
+                                  HouseTags = new SelectList(_searchService.GetTags().Where(x => x.Category == "House"), "DisplayTag", "DisplayTag"),
+                              };
+            
+            if(!string.IsNullOrWhiteSpace(start))
+            {
+                searchModel.SearchResult = _searchService.Search(keywords, memberId, house, business, fromDate.Date, toDate.AddDays(1).AddSeconds(1), page);
+            }
+
+            return View(searchModel);
+        }
+
+        [HttpGet]
+        public virtual PartialViewResult Moments(Guid eventId, string keywords, int? memberId)
+        {
+            var results = _searchService.SearchMoments(eventId, keywords, memberId);
+            var searchMomentModel = new SearchMomentModel(eventId, results, 5);
+            return PartialView(MVC.Search.Views._SearchMoment, searchMomentModel);
+        }
+    }
+}
