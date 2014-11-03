979c35db260fe5852e18505f5d46b67632668369
cairey <chris.airey@vualto.com>
2014-11-03 15:22:11 +0000

    css fix

---
+++ src/UKP.Website.Application/IConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UKP.Website.Application
+{
+    public interface IConfiguration
+    {
+        string IasBaseUrl { get; }
+        string IasAuthKey { get; }
+        string RssUrl { get; }
+        DateTime EpgStartDate { get; }
+    }
+}
