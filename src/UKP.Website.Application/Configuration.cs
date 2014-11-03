c8aafcab9013275038373afe9c88825b0e39fcb6
danielsgroves <danielsgroves@localhost>
2014-11-03 12:46:26 +0000

    Safari bounce fix

---
+++ src/UKP.Website.Application/Configuration.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+
+namespace UKP.Website.Application
+{
+    public class Configuration : IConfiguration
+    {
+        public string IasBaseUrl
+        {
+            get { return ConfigurationManager.AppSettings["IasBaseUrl"]; }
+        }
+
+        public static string GetIasBaseUrl
+        {
+            get { return ConfigurationManager.AppSettings["IasBaseUrl"]; }
+        }
+
+        public string IasAuthKey
+        {
+            get { return ConfigurationManager.AppSettings["IasAuthKey"]; }
+        }
+
+        public static string GetMemberAutocompleteApi
+        {
+            get { return ConfigurationManager.AppSettings["MemberAutocompleteApi"]; }
+        }
+
+        public string RssUrl
+        {
+            get { return ConfigurationManager.AppSettings["RssUrl"]; }
+        }
+
+        public DateTime EpgStartDate
+        {
+            get { return DateTime.Parse(ConfigurationManager.AppSettings["EPGStartDate"]); }
+        }
+
+        public static string GetRssUrl
+        {
+            get { return ConfigurationManager.AppSettings["RssUrl"]; }
+        }
+
+        public static string GetShareTwitter
+        {
+            get { return ConfigurationManager.AppSettings["share-twitter"]; }
+        }
+
+        public static string GetShareFacebook
+        {
+            get { return ConfigurationManager.AppSettings["share-facebook"]; }
+        }
+
+        public static string GetShareGooglePlus
+        {
+            get { return ConfigurationManager.AppSettings["share-google-plus"]; }
+        }
+
+        public static string GetShareLinkedIn
+        {
+            get { return ConfigurationManager.AppSettings["share-linkedin"]; }
+        }
+
+        public static string GetGoogleAnalyticsId
+        {
+            get { return ConfigurationManager.AppSettings["GoogleAnalyticsId"]; }
+        }
+    }
+}
---
+++ src/UKP.Website/Models/SearchViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using UKP.Website.Service.Model;
+
+namespace UKP.Website.Models
+{
+    public class SearchViewModel
+    {
+        public VideoCollectionModel VideoCollectionResult { get; set; }
+
+        public string Business { get; set; }
+        public string House { get; set; }
+
+        public string Keywords { get; set; }
+        public int? MemberId { get; set; }
+
+        public int? Page { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime StartDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime EndDate { get; set; }
+
+    }
+}
