1013897da41659b42311f22acdf6f51818661abd
danielsgroves <danielsgroves@localhost>
2014-11-04 15:33:49 +0000

    EPG Play Icons

---
+++ src/UKP.Website.Application/ProductionSourceConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKP.Website.Application
+{
+    public static class ProductionSourceConstants
+    {
+        public const string TV = "TV";
+        public const string AUDIO = "Audio";
+        public const string AUTOMATED_CAMERA_SYSTEM = "Automated Camera System";
+    }
+}
