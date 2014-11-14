60f5a2c9ad8eec480d0c407bbdd8d3e4aa87d2b6
danielsgroves <danielsgroves@localhost>
2014-11-14 15:28:04 +0000

    Made time period (AM/PM) seperator lower case

--- src/UKP.Website.Application/ApplicationConstants.cs
+++ src/UKP.Website.Application/ApplicationConstants.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace UKP.Website.Application
         public const string DateFormat = "dddd d MMMM h.mmtt";
         public const string YearDateTimeFormat = "dddd d MMMM yyyy h.mmtt";
         public const string YearDateFormat = "dddd d MMMM yyyy";
+        public static DateTimeFormatInfo DateTimeFormatter = new DateTimeFormatInfo() { AMDesignator = "am", PMDesignator = "pm" };
         public const string AcceptCookieName = "Cookies";
     }
 }
