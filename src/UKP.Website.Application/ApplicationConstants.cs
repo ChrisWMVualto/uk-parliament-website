8af7ea80fa5a330e6557fd4365b6e829af948963
cairey <chris.airey@vualto.com>
2014-11-18 14:42:52 +0000

    members query

---
+++ src/UKP.Website.Application/ApplicationConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKP.Website.Application
+{
+    public static class ApplicationConstants
+    {
+        public const string DateFormat = "dddd d MMMM h.mmtt";
+        public const string YearDateTimeFormat = "dddd d MMMM yyyy h.mmtt";
+        public const string YearDateFormat = "dddd d MMMM yyyy";
+        public const string AcceptCookieName = "Cookies";
+    }
+}
---
+++ src/UKP.Website/Content/js/search.js
@@ -0,0 +1,69 @@
+﻿function momentSearch() {
+    var obj = {
+        'parent': $(this).parent().parent(),
+        'buttonCont': $(this).parent(),
+        'url': $(this).attr('data-url-base')
+    };
+
+
+    function init() {
+        obj.buttonCont.remove();
+        fetchResults();
+    }
+
+    function fetchResults() {
+        $.ajax(obj.url, {
+            success: processResults
+        });
+    }
+
+    function processResults(data, textStatus, jqXHR) {
+        obj.parent.append(data);
+    }
+
+    init();
+}
+
+
+
+$(function () {
+
+    $('.moment-more button').on('click', momentSearch);
+
+    $('#Member').autocomplete({
+        serviceUrl: $('#Member').attr('data-ajax-url') + '?name=',
+        displayItem: "DisplayAs",
+        objectPath: "Members.Member",
+        minChars: 3,
+        delimiter: ' ',
+        onSelect: function (member) {
+            $('#MemberId').val(member['@Member_Id']);
+        },
+        noCache: false
+    });
+    
+    $('#Member').change(function () {
+        if ($('#Member').val() == '') {
+            $('#MemberId').val('');
+        }
+    });
+
+
+    $('#searchResultsContainer').append('<span class="pagination"><a href="' + window.location + '"></a></span>');
+    $('#searchResultsContainer').infinitescroll({
+        debug: true,
+        prefill: true,
+        itemSelector: '.search-item',
+        nextSelector: $('.pagination a'),
+        navSelector: $('.pagination'),
+        path: function (pageNum) {
+            return window.location.href + '&page=' + pageNum;
+        },
+        loading: {
+            finishedMsg: '',
+            finished: function() {
+                $('.moment-more button').on('click', momentSearch);
+            }
+        }
+    });
+});
