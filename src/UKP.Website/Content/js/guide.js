50092847467c03b698d2f02d2aa0883cce103588
danielsgroves <danielsgroves@localhost>
2014-11-20 12:02:50 +0000

    Cleanup

--- src/UKP.Website/Content/js/guide.js
+++ src/UKP.Website/Content/js/guide.js
@@ -456,10 +456,14 @@ changeDateTab.prototype = {
                     scrollAmount = $(that.selectors.streamContainer).scrollLeft();
 
                 window.console && console.log('Setting scrollLeft() value to ' + scrollAmount);
-                document.getElementsByClassName('stream-container-inner')[0].scrollLeft = scrollAmount;
-                $(document).off('touchstart', that.disableTouch);
 
+                // More stable than jQuery method
+                document.getElementsByClassName('stream-container-inner')[0].scrollLeft = scrollAmount;
+
+                // Force iOS/Android to redraw
                 $(that.selectors.streamContainer).hide().show(0);
+
+                $(document).off('touchstart', that.disableTouch);
             },
             complete: function () {
                 window.console && console.log('Rebinding event handlers');
