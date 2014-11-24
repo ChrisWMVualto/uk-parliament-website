5cd2d42fa0d329a953b5edeef52044dc0d6cfaf4
danielsgroves <danielsgroves@localhost>
2014-11-24 12:48:41 +0000

    Fixed: EPG - Firefox31 & 32 Safari7 on mac. no marker. Also Opera23 & 24. After further testing I have discovered that the marker is there but the page is opening on a time later than the current time so the marker is hidden.

---
+++ src/UKP.Website/Models/Event/StateChangedModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UKP.Website.Service.Model;
+
+namespace UKP.Website.Models.Event
+{
+    public class StateChangeModel
+    {
+        public Guid EventId { get; set; }
+        public PlanningEventState PlanningState { get; set; }
+        public RecordingEventState RecordingState { get; set; }
+        public RecordedEventState RecordedState { get; set; }
+        public PlayerEventState PlayerState { get; set; }
+        public bool StateChanged { get; set; }
+    }
+}
