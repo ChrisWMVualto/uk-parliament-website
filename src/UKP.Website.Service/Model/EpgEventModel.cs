b625685d96699b4d854458469eb7e6a7bf0f6024
cairey <chris.airey@vualto.com>
2014-12-01 11:00:20 +0000

    merge

--- src/UKP.Website.Service/Model/EpgEventModel.cs
+++ src/UKP.Website.Service/Model/EpgEventModel.cs
@@ -6,10 +6,12 @@ namespace UKP.Website.Service.Model
     public class EpgEventModel
     {
         public EventModel EventData { get; set; }
+        public DateTime BaseDate { get; set; }
 
-        public EpgEventModel(EventModel eventData)
+        public EpgEventModel(EventModel eventData, DateTime baseDate)
         {
             EventData = eventData;
+            BaseDate = baseDate;
         }
 
         public EpgEventModel()
@@ -30,7 +32,13 @@ namespace UKP.Website.Service.Model
 
         public string TemplateLeftPosition()
         {
-            var difference = EventData.DisplayStartDate.ToLocalTime().Subtract(EventData.DisplayStartDate.ToLocalTime().Date);
+            TimeSpan difference;
+
+            if (EventData.DisplayStartDate > BaseDate.Date)
+                difference = EventData.DisplayStartDate.ToLocalTime().Subtract(EventData.DisplayStartDate.ToLocalTime().Date);
+            else
+                difference = EventData.DisplayStartDate.ToLocalTime().Subtract(BaseDate.ToLocalTime().Date);
+
             var left = EventConstants.EPG_MINUTE_SIZE * (int)difference.TotalMinutes;
 
             return string.Format("{0}px", left);
