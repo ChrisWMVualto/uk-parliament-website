3d2bc631baf15a5a06b9aee5400b4d8d8ac874c0
cairey <chris.airey@vualto.com>
2014-12-05 10:25:04 +0000

    change text

---
+++ src/UKP.Website.Service/Model/EpgEventModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UKP.Website.Application;
+
+namespace UKP.Website.Service.Model
+{
+    public class EpgEventModel
+    {
+        public EventModel EventData { get; set; }
+        public DateTime BaseDate { get; set; }
+
+        public EpgEventModel(EventModel eventData, DateTime baseDate)
+        {
+            EventData = eventData;
+            BaseDate = baseDate;
+        }
+
+        public EpgEventModel()
+        {
+            
+        }
+
+        private int TemplateWidthInt()
+        {
+            var difference = EventData.DisplayEndDate.Subtract(EventData.DisplayStartDate);
+            return EventConstants.EPG_MINUTE_SIZE * (int)difference.TotalMinutes;
+        }
+
+        public string TemplateWidth()
+        {
+            return string.Format("{0}px", TemplateWidthInt());
+        }
+
+        public string TemplateLeftPosition()
+        {
+            TimeSpan difference;
+
+            if (EventData.DisplayStartDate > BaseDate.Date)
+                difference = EventData.DisplayStartDate.ToLocalTime().Subtract(EventData.DisplayStartDate.ToLocalTime().Date);
+            else
+                difference = EventData.DisplayStartDate.ToLocalTime().Subtract(BaseDate.ToLocalTime().Date);
+
+            var left = EventConstants.EPG_MINUTE_SIZE * (int)difference.TotalMinutes;
+
+            return string.Format("{0}px", left);
+        }
+
+        public bool SmallItem()
+        {
+            return TemplateWidthInt() <= (90 * EventConstants.EPG_MINUTE_SIZE);
+        }
+    }
+}
---
+++ src/UKP.Website/Controllers/GuideController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Web.Mvc;
+using Date.Extensions;
+using RestSharp.Extensions;
+using UKP.Website.Application;
+using UKP.Website.Models.Guide;
+using UKP.Website.Service;
+
+namespace UKP.Website.Controllers
+{
+    public partial class GuideController : Controller
+    {
+        private readonly IEventService _eventService;
+        private readonly IVideoService _videoService;
+        private readonly IConfiguration _configurationService;
+
+        public GuideController(IEventService eventService, IVideoService videoService, IConfiguration configurationService)
+        {
+            _eventService = eventService;
+            _videoService = videoService;
+            _configurationService = configurationService;
+        }
+
+        [HttpGet]
+        public virtual ActionResult Index()
+        {
+            // TODO: Remove datetime
+            var date = new DateTime(2014, 07, 07);
+            var events = _eventService.GetEpgEvents(date);
+            var model = new GuideViewModel(events, date);
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public virtual PartialViewResult EpgInfo(Guid id)
+        {
+            var result = _videoService.GetVideo(id);
+            return PartialView(MVC.Guide.Views._InfoPopup, result);
+        }
+
+        [HttpGet]
+        public virtual PartialViewResult EpgDay(string date)
+        {
+            var dateob = date.HasValue() ? date.FromISO8601String() : DateTime.Today;
+
+            if (InvalidEpgDate(dateob.Value))
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            var events = _eventService.GetEpgEvents(dateob);
+            var model = new GuideViewModel(events, dateob.Value);
+            return PartialView(MVC.Guide.Views._ChannelListing, model);
+        }
+
+        [HttpGet]
+        public virtual PartialViewResult EpgDateBar(string date)
+        {
+            var dateob = date.HasValue() ? date.FromISO8601String() : DateTime.Today;
+
+            if (InvalidEpgDate(dateob.Value))
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+
+            return PartialView(MVC.Guide.Views._DateTabs, dateob.Value);
+        }
+
+        [HttpGet]
+        public virtual PartialViewResult EpgDayTab(string date, bool previousDay)
+        {
+            return PartialView(MVC.Guide.Views._DateTab, previousDay ? date.FromISO8601String().Value.AddDays(-1) : date.FromISO8601String().Value.AddDays(1));
+        }
+
+        private bool InvalidEpgDate(DateTime date)
+        {
+            return date < _configurationService.EpgStartDate;
+        }
+    }
+}
