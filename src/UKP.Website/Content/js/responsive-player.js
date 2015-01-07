6c80b17632e4008165b9122fcd8eeecbf0635d52
cairey <chris.airey@vualto.com>
2015-01-07 16:03:23 +0000

    auto start

---
+++ src/UKP.Website/Content/js/responsive-player.js
@@ -0,0 +1,11 @@
+﻿function resizePlayer() {
+    var currentWidth = $(".video-container").width();
+    $(".video-container").height((currentWidth / 16) * 9);
+}
+
+$(function () {
+    resizePlayer();
+    $(window).resize(function () {
+        resizePlayer();
+    });
+});
