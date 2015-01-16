31ccfc29d4da3393bd8257c77e90902bc83cecd8
cairey <chris.airey@vualto.com>
2015-01-16 16:53:07 +0000

    feed url

---
+++ src/UKP.Website.Service/Model/VideoCollectionModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace UKP.Website.Service.Model
+{
+    public class VideoCollectionModel
+    {
+        public VideoCollectionModel(IEnumerable<VideoModel> results, int totalCount, int pageSize)
+        {
+            Results = results;
+            TotalCount = totalCount;
+            PageSize = pageSize;
+        }
+
+        public IEnumerable<VideoModel> Results { get; private set; }
+        public int TotalCount { get; private set; }
+        public int PageSize { get; private set; }
+    }
+}
