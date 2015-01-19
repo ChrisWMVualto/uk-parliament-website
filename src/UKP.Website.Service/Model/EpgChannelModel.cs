364b9e31dccbbbc8af47bd223629c2c1f4dd5eeb
cairey <chris.airey@vualto.com>
2015-01-19 12:09:19 +0000

    images

---
+++ src/UKP.Website.Service/Model/EpgChannelModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace UKP.Website.Service.Model
+{
+    public class EpgChannelModel
+    {
+        public int ChannelNum { get; private set; }
+        public List<EpgEventModel> Events { get; private set; }
+
+        public EpgChannelModel(int channelNum, List<EpgEventModel> events)
+        {
+            ChannelNum = channelNum;
+            Events = events;
+        }
+    }
+}
---
+++ src/UKP.Website.Service/Model/RecessMessageType.cs
@@ -0,0 +1,9 @@
+﻿namespace UKP.Website.Service.Model
+{
+    public enum RecessMessageType
+    {
+        HOUSE_OF_COMMONS = 1,
+        HOUSE_OF_LORDS = 2,
+        ALL_COMMITTEES = 3
+    }
+}
