754ec0bf4b7224677a89f1c2fe12155c9af60a77
Daniel Groves <daniel.groves@vualto.com>
2015-01-23 09:20:52 +0000

    Activate datepicker on icon

---
+++ src/UKP.Website.Service/Model/ChannelModel.cs
@@ -0,0 +1,14 @@
+﻿namespace UKP.Website.Service.Model
+{
+    public class ChannelModel
+    {
+        public string Name { get; set; }
+        public string ExternalId { get; set; }
+
+        public ChannelModel(string name, string externalId)
+        {
+            Name = name;
+            ExternalId = externalId;
+        }
+    }
+}
