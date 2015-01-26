aa6b17748afd1494b6e270329832083ece1a8923
Daniel Groves <daniel.groves@vualto.com>
2015-01-26 11:33:30 +0000

    Bundler for CSS

---
+++ src/UKP.Website/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace UKP.Website
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                    "~/Content/css/bootstrap.css",
+                    "~/Content/css/bootstrap-multiselect.css",
+                    "~/Content/css/theme.css",
+                    "~/Content/css/autocomplete.css",
+                    "~/Content/css/application.css"));
+        }
+    }
+}
