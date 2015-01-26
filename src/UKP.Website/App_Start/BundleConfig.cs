4baa6b33a3237f28fdff5620f896f1d3d26319de
Daniel Groves <daniel.groves@vualto.com>
2015-01-26 14:53:39 +0000

    JS Concatination

--- src/UKP.Website/App_Start/BundleConfig.cs
+++ src/UKP.Website/App_Start/BundleConfig.cs
@@ -12,6 +12,29 @@ namespace UKP.Website
                     "~/Content/css/theme.css",
                     "~/Content/css/autocomplete.css",
                     "~/Content/css/application.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/headJs").Include(
+                    "~/Content/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js",
+                    "~/Content/js/vendor/jquery-1.10.1.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/footJs").Include(
+                    "~/Content/js/vendor/jQuery-Mobile.js",
+                    "~/Content/js/vendor/jquery.succinct.min.js",
+                    "~/Content/js/vendor/bootstrap-checkbox.js",
+                    "~/Content/js/vendor/jquery.pep.js",
+                    "~/Content/js/vendor/breakpoints.js",
+                    "~/Content/js/vendor/jquery.slimscroll.js",
+                    "~/Content/js/vendor/slick.js",
+                    "~/Content/js/vendor/bootstrap-timepicker-mod.js",
+                    "~/Content/js/vendor/bootstrap-datepicker.mod.js",
+                    "~/Content/js/vendor/bootstrap-select.js",
+                    "~/Content/js/vendor/bootstrap-multiselect.js",
+                    "~/Content/js/bootstrap.min.js",
+                    "~/Content/js/jquery.autocomplete.mod.js",
+                    "~/Content/js/jquery.infinitescroll.js",
+                    "~/Content/js/scripts.js"
+                ));
         }
     }
 }
