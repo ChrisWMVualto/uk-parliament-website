afca446fd480cc5643015026cf5d7ed8f30d4bb3
cairey <chris.airey@vualto.com>
2015-01-26 14:34:31 +0000

    caching

---
+++ src/UKP.Website/Models/Event/EventVIewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UKP.Website.Application;
+using UKP.Website.Service.Model;
+
+namespace UKP.Website.Models.Event
+{
+    public class EventViewModel
+    {
+        public EventViewModel(VideoModel videoModel)
+        {
+            VideoModel = videoModel;
+        }
+
+        public VideoModel VideoModel { get; private set; }
+
+        public static bool ShowAudioOnly(VideoModel video)
+        {
+            return video.Event.ProductionSource != ProductionSourceConstants.AUDIO
+                                && (video.Event.States.PlayerState == PlayerEventState.LIVE || video.Event.States.PlayerState == PlayerEventState.ARCHIVE)
+                                && video.Event.PublishedStartTime.HasValue;
+        }
+    }
+}
