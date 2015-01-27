baca6ea59a720c17612847af43ef1a82b7daeb46
Daniel Groves <daniel.groves@vualto.com>
2015-01-27 11:25:37 +0000

    CSS cleanup + image optimisations

--- src/UKP.Website/App_Start/BundleConfig.cs
+++ src/UKP.Website/App_Start/BundleConfig.cs
@@ -7,33 +7,31 @@ namespace UKP.Website
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/bundles/css").Include(
-                    "~/Content/css/bootstrap.css",
-                    "~/Content/css/bootstrap-multiselect.css",
-                    "~/Content/css/autocomplete.css",
-                    "~/Content/css/application.css"));
+                "~/Content/css/bootstrap.css",
+                "~/Content/css/bootstrap-multiselect.css",
+                "~/Content/css/autocomplete.css",
+                "~/Content/css/application.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/headJs").Include(
-                    "~/Content/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js",
-                    "~/Content/js/vendor/jquery-1.10.1.js"
-                ));
+                "~/Content/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js",
+                "~/Content/js/vendor/jquery-1.10.1.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/footJs").Include(
-                    "~/Content/js/vendor/jQuery-Mobile.js",
-                    "~/Content/js/vendor/jquery.succinct.min.js",
-                    "~/Content/js/vendor/bootstrap-checkbox.js",
-                    "~/Content/js/vendor/jquery.pep.js",
-                    "~/Content/js/vendor/breakpoints.js",
-                    "~/Content/js/vendor/jquery.slimscroll.js",
-                    "~/Content/js/vendor/slick.js",
-                    "~/Content/js/vendor/bootstrap-timepicker-mod.js",
-                    "~/Content/js/vendor/bootstrap-datepicker.mod.js",
-                    "~/Content/js/vendor/bootstrap-select.js",
-                    "~/Content/js/vendor/bootstrap-multiselect.js",
-                    "~/Content/js/bootstrap.min.js",
-                    "~/Content/js/jquery.autocomplete.mod.js",
-                    "~/Content/js/jquery.infinitescroll.js",
-                    "~/Content/js/scripts.js"
-                ));
+                "~/Content/js/vendor/jQuery-Mobile.js",
+                "~/Content/js/vendor/jquery.succinct.min.js",
+                "~/Content/js/vendor/bootstrap-checkbox.js",
+                "~/Content/js/vendor/jquery.pep.js",
+                "~/Content/js/vendor/breakpoints.js",
+                "~/Content/js/vendor/jquery.slimscroll.js",
+                "~/Content/js/vendor/slick.js",
+                "~/Content/js/vendor/bootstrap-timepicker-mod.js",
+                "~/Content/js/vendor/bootstrap-datepicker.mod.js",
+                "~/Content/js/vendor/bootstrap-select.js",
+                "~/Content/js/vendor/bootstrap-multiselect.js",
+                "~/Content/js/bootstrap.min.js",
+                "~/Content/js/jquery.autocomplete.mod.js",
+                "~/Content/js/jquery.infinitescroll.js",
+                "~/Content/js/scripts.js"));
 
             BundleTable.EnableOptimizations = false;
         }
