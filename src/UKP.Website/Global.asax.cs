0f476a147d7428f0d116de008e1a0489cadd37a4
Daniel Groves <daniel.groves@vualto.com>
2015-01-29 14:31:00 +0000

    Increase EPG title line-spacing

---
+++ src/UKP.Website/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace UKP.Website
+{
+    using System.Web.Optimization;
+
+    using UKP.Website.Extensions;
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalFilters.Filters.Add(new SetBundlerOptimisation());
+        }
+    }
+}
