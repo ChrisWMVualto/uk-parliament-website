61e46551bb0e3b6ab47d4548139ef765a646fb45
Daniel Groves <daniel.groves@vualto.com>
2015-02-04 09:52:08 +0000

    Refactors hard-coded channel loops

---
+++ src/UKP.Website.Service/Model/ChannelModel.cs
@@ -0,0 +1,16 @@
+﻿namespace UKP.Website.Service.Model
+{
+    public class ChannelModel
+    {
+        public string Name { get; set; }
+        public string ExternalId { get; set; }
+        public int InternalId { get; set; }
+
+        public ChannelModel(string name, string externalId, int internalId)
+        {
+            Name = name;
+            ExternalId = externalId;
+            InternalId = internalId;
+        }
+    }
+}
