398f2fbde39c3eea0e831b2a68f2b5734f76b243
cairey <chris.airey@vualto.com>
2015-02-24 16:05:54 +0000

    possible fix for live icon

---
+++ src/UKP.Website/Models/Event/LogUpdateModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UKP.Website.Models.Event
+{
+    public class LogUpdateModel
+    {
+        public Guid EventId { get; set; }
+        public LogUpdateType LogUpdateType { get; set; }
+        public Guid LogMomentId { get; set; }
+        public string Title { get; set; }
+        public DateTime Timecode { get; set; }
+        public string Member { get; set; }
+    }
+
+    public enum LogUpdateType
+    {
+        Add,
+        Update,
+        Delete
+    }
+}
