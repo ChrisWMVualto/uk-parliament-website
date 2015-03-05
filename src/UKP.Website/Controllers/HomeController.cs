f55b226dd9c7572b7d55d694038f20b74ec92dd4
cairey <cairey@localhost>
2015-03-05 13:17:46 +0000

    vary by param

--- src/UKP.Website/Controllers/HomeController.cs
+++ src/UKP.Website/Controllers/HomeController.cs
@@ -22,14 +22,14 @@ namespace UKP.Website.Controllers
         }
 
         [HttpGet]
-        [OutputCache(Duration=45, VaryByParam="none")]
+        [OutputCache(Duration=45, VaryByParam="*")]
         public virtual ActionResult Index()
         {
             return RedirectToAction(MVC.Home.Commons());
         }
 
         [HttpGet]
-        [OutputCache(Duration=45, VaryByParam="none")]
+        [OutputCache(Duration=45, VaryByParam="*")]
         public virtual ActionResult Commons()
         {
             var model = new HomeViewsModel(_eventService.GetNowEvents(), _eventService.GetMiniGuide(), _recessService.GetRecessMessage(RecessMessageType.HOUSE_OF_COMMONS), EventFilter.COMMONS);
@@ -37,7 +37,7 @@ namespace UKP.Website.Controllers
         }
 
         [HttpGet]
-        [OutputCache(Duration=45, VaryByParam="none")]
+        [OutputCache(Duration=45, VaryByParam="*")]
         public virtual ActionResult Lords()
         {
             var model = new HomeViewsModel(_eventService.GetNowEvents(EventFilter.LORDS), _eventService.GetMiniGuide(EventFilter.LORDS), _recessService.GetRecessMessage(RecessMessageType.HOUSE_OF_LORDS), EventFilter.LORDS);
@@ -45,7 +45,7 @@ namespace UKP.Website.Controllers
         }
 
         [HttpGet]
-        [OutputCache(Duration=45, VaryByParam="none")]
+        [OutputCache(Duration=45, VaryByParam="*")]
         public virtual ActionResult Committees()
         {
             var model = new HomeViewsModel(_eventService.GetNowEvents(EventFilter.COMMITTEES), _eventService.GetMiniGuide(EventFilter.COMMITTEES), _recessService.GetRecessMessage(RecessMessageType.ALL_COMMITTEES), EventFilter.COMMITTEES);
@@ -66,7 +66,7 @@ namespace UKP.Website.Controllers
         }
 
 
-        [OutputCache(Duration=45, VaryByParam="none")]
+        [OutputCache(Duration=45, VaryByParam="*")]
         public ActionResult Robots()
         {
             var robotsFile = "~/robots.txt.disallow";
