dc62c9f983a842fe8f5e3d7f0aca999748487f7d
cairey <cairey@localhost>
2015-03-06 00:41:48 +0000

    combat mode

--- src/UKP.Website/Controllers/EventController.cs
+++ src/UKP.Website/Controllers/EventController.cs
@@ -70,7 +70,7 @@ namespace UKP.Website.Controllers
         }
 
         [HttpGet]
-        [OutputCache(Duration=10, VaryByParam="*", Location = OutputCacheLocation.ServerAndClient)]
+        [OutputCache(Duration=10, VaryByParam="*")]
         public virtual PartialViewResult EventTitle(Guid id, string @in = null, string @out = null)
         {
             var inPoint = @in.FromISO8601String();
