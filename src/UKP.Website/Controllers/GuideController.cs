831603c38ebdbcf6bf6567e2779855ca7269f3de
cairey <chris.airey@vualto.com>
2015-03-10 10:57:01 +0000

    vary by custom

--- src/UKP.Website/Controllers/GuideController.cs
+++ src/UKP.Website/Controllers/GuideController.cs
@@ -23,7 +23,7 @@ namespace UKP.Website.Controllers
         }
 
         [HttpGet]
-        [OutputCache(Duration=60, VaryByParam="*")]
+        [OutputCache(Duration=60, VaryByCustom="*")]
         public virtual ActionResult Index()
         {
             var date = DateTime.Today;
