77834f1bc7e591c207704e085f2518d65855a915
cairey <chris.airey@vualto.com>
2015-03-12 11:42:13 +0000

    fix for player rezise issue

--- src/UKP.Website/Content/js/responsive-player.js
+++ src/UKP.Website/Content/js/responsive-player.js
@@ -1,10 +1,17 @@
 ﻿function resizePlayer() {
     var currentWidth = $(".video-container").width();
     $(".video-container").height(Math.ceil((currentWidth / 16) * 9));
+    $(".video-container iframe").css("height", "100%");
 }
 
 $(function () {
     resizePlayer();
+
+    setTimeout(function() {
+        resizePlayer();
+    }, 1000);
+
+    
     $(window).resize(function () {
         resizePlayer();
     });
