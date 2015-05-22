653b49fe1226924ea8a0cea54c79e8ce21c62a4c
cairey <cairey@localhost>
2015-05-22 10:18:42 +0100

    member search

--- src/UKP.Website/Controllers/SearchController.cs
+++ src/UKP.Website/Controllers/SearchController.cs
@@ -50,7 +50,7 @@ namespace UKP.Website.Controllers
                     ModelState.AddModelError("dates", "End date cannot occur before the start date.");
             }
 
-            if(!memberId.HasValue) keywords += " " + member;
+            if(!memberId.HasValue && !firstSearchLoad) keywords += " " + "\"" + member + "\"";
 
             var searchModel = new SearchViewModel()
             {
