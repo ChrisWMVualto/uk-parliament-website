501c77f550ed1760bfec1127e389da0bdf105136
cairey <cairey@localhost>
2015-07-31 10:31:18 +0100

    responsive script

--- src/UKP.Website/Content/js/responsive-player.js
+++ src/UKP.Website/Content/js/responsive-player.js
@@ -1,7 +1,7 @@
 ﻿function resizePlayer() {
     var currentWidth = $(".video-container").width();
     $(".video-container").height(Math.ceil((currentWidth / 16) * 9));
-    $(".video-container iframe").css("height", "100%");
+    $(".video-container iframe").css("height", "100.01%");
 }
 
 $(function () {
