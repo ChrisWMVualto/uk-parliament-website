a6f44cb922664c79620b10db27e9d11ad38bbc1c
Corrie Barton <corrie.barton@vualto.com>
2017-09-11 15:25:25 +0000

    Close branch UKP-39-download-service-layer

---
+++ src/UKP.Website.Service/Model/LogMomentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UKP.Website.Service.Model
+{
+    public class LogMomentModel
+    {
+        public LogMomentModel(Guid id, string description, string thumbnailUrl, DateTime? inPoint, string iasDisplayAs, string webPageUrl)
+        {
+            Id = id;
+            Description = description;
+            ThumbnailUrl = thumbnailUrl;
+            InPoint = inPoint;
+            IasDisplayAs = iasDisplayAs;
+            WebPageUrl = webPageUrl;
+        }
+
+        public Guid Id { get; private set; }
+        public string Description { get; private set; }
+        public string ThumbnailUrl { get; private set; }
+        public DateTime? InPoint { get; private set; }
+        public string IasDisplayAs { get; private set; }
+        public string WebPageUrl { get; private set; }
+    }
+}
