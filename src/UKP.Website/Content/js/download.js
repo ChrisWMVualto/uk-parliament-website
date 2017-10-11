b26e5784c53974ebae92b9538581bffcf22bcbed
owen_hallett <owen_hallett@localhost>
2017-10-11 15:04:05 +0100

    Added javascript files to retrieve streamUrl and times from the player iframe

---
+++ src/UKP.Website/Content/js/download.js
@@ -0,0 +1,15 @@
+﻿function createDownload() {
+    var form = $("#createDownloadForm");
+    var data = $(form).serialize();
+
+    $.ajax({
+        method: 'POST',
+        url: "/Event/CreateDownload",
+        data: data,
+        success: function (data) {
+            var response = JSON.parse(data);
+            alert(response.Message);
+        }
+    });
+
+}
---
+++ src/UKP.Website/Models/Home/HomeViewsModel.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UKP.Website.Service.Model;
+
+namespace UKP.Website.Models.Home
+{
+    public class HomeViewsModel
+    {
+        public HomeViewsModel(NowAndNextModel nowNext, IEnumerable<EventModel> epg, RecessMessageModel recessMessage, EventFilter eventFilter)
+        {
+            NowNext = nowNext;
+            Epg = epg;
+            RecessMessage = recessMessage;
+            EventFilter = eventFilter;
+        }
+
+        public NowAndNextModel NowNext { get; private set; }
+        public IEnumerable<EventModel> Epg { get; private set; }
+        public IEnumerable<EventModel> RecentlyArchived { get; private set; }
+        public RecessMessageModel RecessMessage { get; private set; }
+        public EventFilter EventFilter { get; private set; }
+    }
+}
