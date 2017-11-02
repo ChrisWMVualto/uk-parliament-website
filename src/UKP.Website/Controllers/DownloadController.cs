dc8d84e60e15bc926a0afcd7d2ca7c9564bcb24d
owen_hallett <owen_hallett@localhost>
2017-11-02 14:40:12 +0000

    Added download controller

---
+++ src/UKP.Website/Controllers/DownloadController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UKP.Website.Controllers
+{
+    public class DownloadController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(Guid id)
+        {
+
+
+            return Redirect("https://www.google.com");
+        }
+    }
+}
