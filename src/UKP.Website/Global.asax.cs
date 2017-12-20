6fb2c74fac031d35a345fc8a6fc21b68af0447cf
owen_hallett <owen_hallett@localhost>
2017-12-20 11:12:47 +0000

    Added timeout call to get stream url from player, removed unused method

---
+++ src/UKP.Website/Global.asax.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System.Web.Optimization;
+using UKP.Website.Extensions;
+
+namespace UKP.Website
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalFilters.Filters.Add(new SetBundlerOptimisation());
+        }
+
+        public override string GetVaryByCustomString(HttpContext context, string arg)
+        {
+            if(arg.ToLower() == "*")
+            {
+                return BrowserFilterAttribute.IsSupported().ToString() + Request.QueryString + Request.Form;
+            }
+            return base.GetVaryByCustomString(context, arg);
+        }
+    }
+}
