45c0189804c44bea2b9a8995c5c4e12b32c087af
shlegelaa <andrey.shlegel@oxagile.com>
2017-12-26 10:54:59 +0300

    Not remove .aspx handlers what required for debugging process.

---
+++ src/UKP.Website/Content/js/groovy.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener("message", updateCurrentTime);
+
+function updateCurrentTime(event) {
+    var messageSplit = event.data.split("_");
+    document.getElementById("ProgramDateTime").value = messageSplit[1];
+}
+
+function getTime(elementId) {
+    var time = new Date(document.getElementById("ProgramDateTime").value);
+    document.getElementById(elementId).value = time.toJSON();
+}
+
+function getStreamUrlOnLoad() {
+    //this is here to stop a console error
+}
