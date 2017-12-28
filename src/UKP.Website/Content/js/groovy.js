c7f5bf80081397dbe339f64732849fd51c3a589c
shlegelaa <andrey.shlegel@oxagile.com>
2017-12-28 14:36:07 +0300

    Set alternativeHostName to specifies the host name to use for redirection

---
+++ src/UKP.Website/Content/js/groovy.js
@@ -0,0 +1,44 @@
+﻿$(function() {
+    enableAudioOnly();
+});
+
+function enableAudioOnly() {
+    var radio = $("#fileType2");
+    radio.removeAttr("disabled");
+}
+
+function getTime() {
+    return new Date(document.getElementById("ProgramDateTime").value);
+}
+
+function getShareTime(e) {
+
+    var timeString = getTime().toTimeString();
+    var textbox = document.getElementById(e.target.dataset.textboxId);
+    textbox.value = timeString.split(' ')[0];
+    textbox.dataset.lastInput = textbox.value;
+
+    reloadEmbedData();
+}
+
+function getDownloadTime(e) {
+    var time = getTime();
+    var timeString = time.toTimeString();
+    var textbox = document.getElementById(e.target.dataset.textboxId);
+    textbox.value = timeString.split(' ')[0];
+    textbox.dataset.lastInput = textbox.value;
+    document.getElementById(e.target.dataset.formId).value = time.toJSON();
+
+    if (e.target.id === "downloadStartTimeSet") {
+        checkStartTime();
+    }
+    else if (e.target.id === "downloadEndTimeSet") {
+        checkEndTime();
+    }
+
+}
+
+function initSetDownloadTime(e) {
+    var time = getTime();
+    document.getElementById(e.target.dataset.formId).value = time.toJSON();
+}
