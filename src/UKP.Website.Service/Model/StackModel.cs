cfb8f96c3e884b185ab6c667e47ade06ac6fcd8c
Corrie Barton <corrie.barton@vualto.com>
2018-03-06 14:43:57 +0000

    Added transforms for live recaptcha keys

---
+++ src/UKP.Website.Service/Model/StackModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UKP.Website.Service.Model
+{
+    public class StackModel
+    {
+        public StackModel(Guid id, string description, string iasDisplayAs, int? sortOrder)
+        {
+            Id = id;
+            Description = description;
+            IasDisplayAs = iasDisplayAs;
+            SortOrder = sortOrder;
+        }
+
+        public Guid Id { get; private set; }
+        public string Description { get; private set; }
+        public string IasDisplayAs { get; private set; }
+        public int? SortOrder { get; private set; }
+    }
+}
---
+++ src/UKP.Website/Models/Event/LogUpdateModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UKP.Website.Models.Event
+{
+    public class LogUpdateModel
+    {
+        public Guid EventId { get; set; }
+        public LogUpdateType LogUpdateType { get; set; }
+        public Guid LogMomentId { get; set; }
+        public string Description { get; set; }
+        public DateTime Timecode { get; set; }
+        public string DisplayAs { get; set; }
+
+
+    }
+
+    public enum LogUpdateType
+    {
+        Add,
+        Update,
+        Delete
+    }
+}
