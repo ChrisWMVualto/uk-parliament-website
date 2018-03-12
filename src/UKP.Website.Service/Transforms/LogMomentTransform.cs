55606dc4fbca860415a61677bb327f58edf9a4af
Corrie Barton <corrie.barton@vualto.com>
2018-03-12 10:07:56 +0000

    Replaced Privilege url with privacy statement on download tab, updated nuget packages

---
+++ src/UKP.Website.Service/Transforms/LogMomentTransform.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using UKP.Website.Service.Model;
+
+namespace UKP.Website.Service.Transforms
+{
+    public static class LogMomentTransforms
+    {
+        public static IEnumerable<LogMomentModel> TransformArray(dynamic jsonArray)
+        {
+            if(jsonArray == null) return Enumerable.Empty<LogMomentModel>();
+
+            var list = new List<LogMomentModel>();
+            foreach(var json in jsonArray)
+            {
+                list.Add(Transform(json));
+            }
+
+            return list;
+        }
+
+        public static LogMomentResultModel TransformObject(string jsonObject)
+        {
+            dynamic jObject = JObject.Parse(jsonObject);
+
+            var logMoments = Enumerable.Empty<LogMomentModel>();
+            var total = (int) jObject.totalResults;
+            var containsLogMoments = (bool)jObject.containsLogMoments;  
+            if (total > 0) logMoments = TransformArray(jObject.results);
+            var searchHighlightList = SearchHighlightTransforms.TransformArray(jObject.searchHighlights.ToString());
+
+            return new LogMomentResultModel(logMoments, total, containsLogMoments, searchHighlightList);
+        }
+
+        public static LogMomentModel Transform(dynamic json)
+        {
+            var id = (Guid)json.log.id;
+            var description = (string)json.log.description;
+            var thumbnailUrl = (string)json.thumbnailImageUrl;
+            var inPoint = (DateTime?)json.inPoint;
+            var iasDisplayAs = (string)json.log.iasDisplayAs;
+            var webPageUrl = (string)json.webPageUrl;
+
+            return new LogMomentModel(id, description, thumbnailUrl, inPoint, iasDisplayAs, webPageUrl);
+        }
+    }
+}
---
+++ src/UKP.Website/Models/Guide/GuideViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UKP.Website.Application;
+using UKP.Website.Service.Model;
+
+namespace UKP.Website.Models.Guide
+{
+    public class GuideViewModel
+    {
+        public GuideViewModel(IEnumerable<EpgChannelModel> events, DateTime date, IEnumerable<ChannelModel> channels)
+        {
+            Events = events;
+            BaseDate = date;
+            Channels = channels;
+        }
+
+        public GuideViewModel(IEnumerable<EpgChannelModel> events, IEnumerable<ChannelModel> channels)
+            : this(events, DateTime.Now, channels)
+        {
+            
+        }
+
+        public int LivePosition()
+        {
+            var timespan = DateTime.Now.ToLocalTime().Subtract(DateTime.Today);
+            return Convert.ToInt32(timespan.TotalMinutes * EventConstants.EPG_MINUTE_SIZE);
+        }
+
+        public IEnumerable<EpgChannelModel> Events { get; private set; }
+        public DateTime BaseDate { get; private set; }
+        public IEnumerable<ChannelModel> Channels { get; private set; }
+    }
+}
