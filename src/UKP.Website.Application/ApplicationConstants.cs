8bf30e9592fd52418600a35b5f2079b167e8c2c0
Corrie Barton <corrie.barton@vualto.com>
2018-05-01 14:19:39 +0100

    Removes space delimiter on autocomplete

---
+++ src/UKP.Website.Application/ApplicationConstants.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace UKP.Website.Application
+{
+    public static class ApplicationConstants
+    {
+        public const string DateFormat = "dddd d MMMM h.mmtt";
+        public const string YearDateTimeFormat = "dddd d MMMM yyyy h.mmtt";
+        public const string YearDateFormat = "dddd d MMMM yyyy";
+        public const string YearDateTimeWithoutMeridiemFormat = "dddd d MMMM yyyy h.mm";
+        public const string MerdiemFormat = "tt";
+        public static DateTimeFormatInfo DateTimeFormatter = new DateTimeFormatInfo() { AMDesignator = "am", PMDesignator = "pm" };
+        public const string AcceptCookieName = "Cookies";
+        public const int EpgItemMinWidth = 240;
+    }
+}
---
+++ src/UKP.Website.Service/Model/DownloadUrlModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKP.Website.Service.Model
+{
+    public class DownloadUrlModel
+    {
+        public DownloadUrlModel(Guid id, string url)
+        {
+            Id = id;
+            Url = url;
+        }
+
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+    }
+}
