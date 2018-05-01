fa2676473a62295026bf434b7bdd0e17f257eb7b
Corrie Barton <corrie.barton@vualto.com>
2018-05-01 10:17:33 +0100

    Removes cache control custom headers

---
+++ src/UKP.Website/Content/js/download.js
@@ -0,0 +1,68 @@
+﻿function createDownload() {
+
+    var clipRequested = document.getElementById("ClipRequested");
+
+    if (clipRequested.value === true) {
+        return;
+    }
+
+    isValidCaptcha();
+
+    if (!compareTimes()) {
+        return;
+    }
+
+    var endTime = document.getElementById("EndTime");
+    if (endTime.value === "") {
+        endTime.value = document.getElementById("MeetingEndTime").value;
+    }
+
+    var form = $("#createDownloadForm");
+    var data = $(form).serialize();
+
+    $(".error-message").prop("hidden", true);
+    $("#downloadSubmit").prop("disabled", true);
+
+    clipRequested.value = true;
+
+    $.ajax({
+        method: 'POST',
+        url: "/Event/CreateDownload",
+        data: data,
+        success: function (data) {
+            var response = JSON.parse(data);
+
+            if (response.Success) {
+                $(".thankyou-header").text("Thank you");
+                $(".thankyou-message").text(response.Message);
+                $(".thankyou-email").text(response.Email);
+
+                $(".download-form").prop("hidden", true);
+                $(".thankyou").removeAttr("hidden");
+
+                urlPageNavigation("#player-tabs");
+
+            } else {
+                $(".error-message").removeAttr("hidden");
+                $(".error-message").text(response.Message);
+                keepError = true;
+                clipRequested.value = false;
+                grecaptcha.reset();
+            }
+        }
+    });
+
+}
+
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event) {
+    var message = JSON.parse(event.data);
+    var sender = $('#UKPPlayer')[0].src;
+    if (message.sender === sender) window[message.function](message.data);
+}
+
+function updateCurrentTime(data) {
+    var messageSplit = data.currentTime.split("_");
+    document.getElementById("ProgramDateTime").value = messageSplit[1];
+}
---
+++ src/UKP.Website/Content/js/search.js
@@ -0,0 +1,76 @@
+﻿function momentSearch() {
+    var obj = {
+        'parent': $(this).parent().parent(),
+        'buttonCont': $(this).parent(),
+        'url': $(this).attr('data-url-base')
+    };
+
+    function init() {
+        obj.buttonCont.remove();
+        fetchResults();
+    }
+
+    function fetchResults() {
+        $.ajax(obj.url, {
+            success: processResults
+        });
+    }
+
+    function processResults(data, textStatus, jqXHR) {
+        obj.parent.append(data);
+    }
+
+    init();
+}
+
+
+$(function () {
+
+    $('.moment-more button').on('click', momentSearch);
+
+    $('#Member').keypress(function () {
+        $('#MemberId').val('');
+    });
+
+    $('#Member').autocomplete({
+        serviceUrl: $('#Member').attr('data-ajax-url') + '?name=',
+        displayItem: "Member",
+        minChars: 3,
+        delimiter: ' ',
+        onSelect: function (member) {
+            $('#MemberId').val(member['MembersNameId']);
+        },
+        noCache: false
+    });
+
+
+    $('#searchResultsContainer').append('<span class="pagination"><a href="' + window.location + '"></a></span>');
+    $('#searchResultsContainer').infinitescroll({
+        debug: true,
+        prefill: true,
+        itemSelector: '.search-item',
+        nextSelector: $('.pagination a'),
+        navSelector: $('.pagination'),
+        path: function (pageNum) {
+            return window.location.href + '&page=' + pageNum;
+        },
+        loading: {
+            finishedMsg: '',
+            finished: function () {
+                $('.moment-more button').on('click', momentSearch);
+            },
+            msgText: ''
+        }
+    });
+
+
+    $('.start-date').datepicker().on('changeDate', function (e) {
+
+        $('.end-date').datepicker('update', e.date);
+    });
+
+
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        $('#Start, #End').attr('readonly', true);
+    }
+});
