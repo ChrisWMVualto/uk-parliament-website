89b4193f31f93ad8dfa7ae62242bc5cc3beed2dc
Corrie Barton <corrie.barton@vualto.com>
2018-05-11 13:20:40 +0100

    Adds security headers

---
+++ src/UKP.Website/Models/Event/CaptchaVerifyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UKP.Website.Models.Event
+{
+    public class CaptchaVerifyModel
+    {
+        public string Response { get; set; }
+
+        public CaptchaVerifyModel(string response)
+        {
+            Response = response;
+        }
+    }
+}
