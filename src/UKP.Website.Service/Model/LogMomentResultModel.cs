fab7cbea4afa0fa30442172160f9b533db3dccb2
Corrie Barton <corrie.barton@vualto.com>
2018-05-17 09:19:05 +0100

    compareTimes is now ran when the dropdowns are updated

---
+++ src/UKP.Website.Service/Model/LogMomentResultModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace UKP.Website.Service.Model
+{
+    public class LogMomentResultModel
+    {
+        public LogMomentResultModel(IEnumerable<LogMomentModel> results, int total, bool containsLogMoments, IEnumerable<SearchHighlightCollectionModel> searchHighlights)
+        {
+            Results = results;
+            Total = total;
+            ContainsLogMoments = containsLogMoments;
+            SearchHighlights = searchHighlights;
+        }
+
+        public IEnumerable<LogMomentModel> Results { get; private set; }
+        public int Total { get; private set; }
+        public bool ContainsLogMoments { get; private set; }
+        public IEnumerable<SearchHighlightCollectionModel> SearchHighlights { get; private set; }
+    }
+}
