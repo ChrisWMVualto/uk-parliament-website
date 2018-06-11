54aad78fd48c4e120bda207f9686ed15b310fc6b
owen_hallett <owen_hallett@localhost>
2018-06-11 10:41:59 +0100

    Moved javascript to new files and bundles to prevent console errors appearing for meetings where download is disabled

---
+++ src/UKP.Website.Application/IConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UKP.Website.Application
+{
+    public interface IConfiguration
+    {
+        string IasBaseUrl { get; }
+        string IasAuthKey { get; }
+        string RssUrl { get; }
+        DateTime EpgStartDate { get; }
+        string GetMemberAutocompleteApi { get; }
+        bool RobotsAllow { get; }
+        string GoogleRecaptchaVerifyUrl { get; }
+        string RecaptchaSecret { get; }
+    }
+}
