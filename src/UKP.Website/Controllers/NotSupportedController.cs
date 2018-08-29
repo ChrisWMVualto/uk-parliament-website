945ce7175ba4782dc8cfe20c2050f1e830628e59
Corrie Barton <corrie.barton@vualto.com>
2018-08-29 16:31:08 +0100

    adds parliamentlive.tv to connect-src on live config

---
+++ src/UKP.Website/Controllers/NotSupportedController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using UKP.Website.Extensions;
+
+namespace UKP.Website.Controllers
+{
+    public partial class NotSupportedController : Controller
+    {
+        [HttpGet]
+        [SkipBrowserFilter]
+        [RequireHttps]
+        public virtual ActionResult Index()
+        {
+            Response.StatusCode = 403;
+            return View();
+        }
+    }
+}
